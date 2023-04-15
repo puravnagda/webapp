a6d25d277821e977095b0eb947e055ec87e3c1da
Purav Nagda <pnagda426@gmail.com>
2023-04-15 12:04:33 +0530

    Website in progress

---
+++ web-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import './styles.css'
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1 h1 className="gradient-text">Wealthians Finserv</h1>
+      <h3>Financial Planning Services</h3>
+      <p>This website is currently in progress, and so is your financial growth.</p>
+    </div>
+  );
+}
+
+export default App;
